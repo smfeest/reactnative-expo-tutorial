153bca78563157ccd09711ebd906b934b02f3279
Stephen Feest <stephen.feest@futurelearn.com>
2021-02-19 09:52:36 +0000

    Styling exercise

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { FunctionComponent } from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 const App: FunctionComponent = () => (
-  <SafeAreaView style={styles.container}>
-    <Text>Open up App.tsx to start working on your app!</Text>
-    <StatusBar style="auto" />
+  <SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.intro}>Here are some boxes of different colours</Text>
+      <Text style={[styles.colorBox, styles.cyan]}>Cyan #2aa198</Text>
+      <Text style={[styles.colorBox, styles.blue]}>Blue #268bd2</Text>
+      <Text style={[styles.colorBox, styles.magenta]}>Magenta #d33682</Text>
+      <Text style={[styles.colorBox, styles.orange]}>Orange #cb4b16</Text>
+    </View>
   </SafeAreaView>
 );
 
@@ -13,8 +17,29 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    padding: 15,
+  },
+  intro: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  colorBox: {
+    borderRadius: 3,
+    color: '#fff',
+    marginTop: 10,
+    padding: 5,
+    textAlign: 'center',
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
   },
 });
