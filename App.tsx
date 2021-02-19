cfb8e87011277f5675d8444024173d62c52b21b9
Stephen Feest <stephen.feest@futurelearn.com>
2021-02-19 09:34:08 +0000

    Use arrow function for App component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App: FunctionComponent = () => (
+  <View style={styles.container}>
+    <Text>Open up App.tsx to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>
+);
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
