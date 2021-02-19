6e3c19c3e39663b0722601c3a54c647967c659e8
Stephen Feest <stephen.feest@futurelearn.com>
2021-02-19 11:33:28 +0000

    Use FlatList

--- App.tsx
+++ App.tsx
@@ -1,19 +1,31 @@
 import React, { FunctionComponent } from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 import ColorBox from './components/ColorBox';
 
-const App: FunctionComponent = () => (
-  <SafeAreaView>
-    <View style={styles.container}>
-      <Text style={styles.intro}>Here are some boxes of different colours</Text>
-      <ColorBox colorName="Cyan" hexCode="#2aa198" />
-      <ColorBox colorName="Blue" hexCode="#268bd2" />
-      <ColorBox colorName="Magenta" hexCode="#d33682" />
-      <ColorBox colorName="Orange" hexCode="#cb4b16" />
-    </View>
-  </SafeAreaView>
-);
+const App: FunctionComponent = () => {
+  const colorData = [
+    { colorName: 'Cyan', hexCode: '#2aa198' },
+    { colorName: 'Blue', hexCode: '#268bd2' },
+    { colorName: 'Magenta', hexCode: '#d33682' },
+    { colorName: 'Orange', hexCode: '#cb4b16' },
+  ];
+
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.intro}>
+          Here are some boxes of different colours
+        </Text>
+        <FlatList
+          data={colorData}
+          renderItem={({ item }) => <ColorBox {...item} />}
+          keyExtractor={(item) => item.hexCode}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default App;
 
